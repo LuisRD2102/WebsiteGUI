8321a45fb47ca6bfb41c59ef6394f1436cbeb512
LuisRD2102 <105602753+LuisRD2102@users.noreply.github.com>
2023-08-25 09:50:00 -0400

    Adelanto landing

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,19 +1,20 @@
 <template>
     <div
-        class="relative h-screen overflow-hidden bg-cover bg-[50%] bg-no-repeat">
-        <img
-            src="../assets/img/space.jpg"
-            class="h-screen m-0 w-full" />
-        <div
-            class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
-            style="background-color: hsla(0, 0%, 0%, 0.6)">
-            <div class="flex h-full items-center justify-center">
-                <h1 class="mb-2 mt-0 text-5xl font-medium leading-tight text-white">
-                    Tailwind Elements
-                </h1>
-            </div>
+    class="relative h-screen overflow-hidden bg-contain bg-[50%] bg-no-repeat">
+    <img
+        src="../assets/img/Landing.png"
+        class="h-screen m-0 w-full" />
+    <div
+        class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+        >
+        <div class="flex h-full items-center justify-center">
+            <h1 class="mb-2 mt-0 text-5xl font-medium leading-tight text-black">
+                Tailwind Elements
+            </h1>
         </div>
     </div>
+</div>
+
 </template>
 
 <script setup>
