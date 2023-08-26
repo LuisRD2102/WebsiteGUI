84684f5b94d2e6304f18e46ed03cddc0808d9e91
LuisRD2102 <105602753+LuisRD2102@users.noreply.github.com>
2023-08-26 19:56:13 -0400

    fix button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,29 +1,30 @@
 <template>
-    <button
-      type="button"
-      class="flex gap-2 items-center inline-block rounded bg-primary p-2 text-xs h-9 font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#e4a11b] transition duration-150 ease-in-out hover:bg-warning-600 hover:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] focus:outline-none focus:ring-0 active:bg-warning-700 active:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.3),0_4px_18px_0_rgba(228,161,27,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(228,161,27,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)]"
-    >
-      <i class="material-icons-outlined text-lg text-white">{{ icon }}</i>
-      {{ label }}
-    </button>
-  </template>
-  
-  <script setup>
-  defineProps({
-    icon: {
-      type: String,
-      required: false,
-    },
-    label: {
-      type: String,
-      required: true,
-    },    
-    url: {
+  <button
+    type="button"
+    :class="['inline-block rounded bg-primary p-2 text-xs h-9 font-bold uppercase leading-normal text-white shadow-[0_4px_9px_-4px_black] transition duration-150 ease-in-out hover:bg-warning-600 hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)] focus:bg-warning-600 focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:bg-warning-700 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.3),0_4px_18px_0_rgba(0,0,0,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(228,161,27,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(228,161,27,0.2),0_4px_18px_0_rgba(228,161,27,0.1)]', icon ? 'flex gap-2 items-center' : '']"
+  >
+    <i v-if="icon" class="material-icons-outlined text-lg text-white">{{ icon }}</i>
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  icon: {
     type: String,
     required: false,
   },
-  });
-  </script>
-  
-  <style></style>
+  label: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<style></style>
+
   
