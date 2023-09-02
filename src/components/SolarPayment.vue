a60ce63ba7000e11ad22240a2c9dec3dfc035ccb
santiagovm2000 <vieirasantiagovieira@gmail.com>
2023-09-01 20:41:25 -0400

    Fix bugs de graficas necesario refrescar

--- src/components/SolarPayment.vue
+++ src/components/SolarPayment.vue
@@ -16,8 +16,8 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 pb-3">
         <div class="w-full border-gray-200 border-r">
             <div class="ps-1 pb-3 pt-12">
-                <p class="text-center text-sm font-semibold">Your current bill <span
-                        class="c-primary">{{ currentBill }}$</span></p>
+                <p class="text-center text-sm font-semibold">Your current bill <span class="c-primary">{{ currentBill
+                }}$</span></p>
 
             </div>
             <div class="flex flex-col space-y-2 p-2 px-3">
@@ -27,7 +27,7 @@
                 </ul>
             </div>
             <div class="ps-1 pb-3 pt-8">
-                <p class="text-center text-sm font-semibold">Current cost per kWh: ${{currentCostPerKwh}}/kWh</p>
+                <p class="text-center text-sm font-semibold">Current cost per kWh: ${{ currentCostPerKwh }}/kWh</p>
             </div>
 
             <div class="pt-6 w-full">
@@ -40,10 +40,10 @@
             </div>
 
         </div>
-        <div id="bar-chart" class="w-full "></div>
+        <div id="bar-chart" class="w-full " :key="barChartoptions"></div>
         <div style="position: relative;" class="w-full flex justify-center">
             <div class="w-full">
-                <div id="pie-chart"></div>
+                <div id="pie-chart" :key="pieChartOptions"></div>
                 <!-- <div class="text-center">Solar offset 125%</div> -->
                 <p class="text-center text-sm font-semibold">Solar offset 125%</p>
             </div>
@@ -56,133 +56,127 @@
 
 <script>
 export default {
-  data() {
-    return {
-        currentBill: 120,
-        yourPayment: 172,
-        barChart: null,
-        currentCostPerKwh: 0.0247
-    };
-  },
-  mounted() {
-    window.addEventListener("load", this.initializeCharts);
-  },
-  methods: {
-    initializeCharts() {
-      const slider = document.getElementById('myRange');
-      const labelsContainer = document.getElementById('sliderLabels');
+    data() {
+        return {
+            currentBill: 120,
+            yourPayment: 172,
+            barChart: null,
+            currentCostPerKwh: 0.0247,
+            barChartoptions: {
+                series: [
+                    {
+                        name: "Current Bill",
+                        data: [120]
+                    },
+                    {
+                        name: "Your Payment",
+                        data: [172]
+                    }
+                ],
+                colors: ["#1A56DB", "#FDBA8C"],
+                chart: {
+                    type: 'bar',
+                    height: 350,
+                    toolbar: {
+                        show: false,
+                    },
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        columnWidth: "50%",
+                        borderRadiusApplication: "end",
+                        borderRadius: 45,
+                    }
+                },
+                grid: {
+                    show: false
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                xaxis: {
+                    categories: ['Bill'],
+                }
+            },
+            pieChartOptions: {
+                series: [30, 76],
+                chart: {
+                    type: 'pie',
+                    height: 325,
 
-      const min = parseInt(slider.min);
-      const max = parseInt(slider.max);
-      const step = (max - min) / 5;  // Divide into 6 parts
-
-      for (let i = min; i <= max; i += step) {
-          const li = document.createElement('li');
-          li.className = 'flex justify-center relative';
-          li.innerHTML = `<span class="absolute">${Math.round(i)}</span>`;
-          labelsContainer.appendChild(li);
-      }
-
-
-      const barChartoptions = {
-          series: [
-              {
-                  name: "Current Bill",
-                  data: [this.currentBill]
-              },
-              {
-                  name: "Your Payment",
-                  data: [this.yourPayment]
-              }
-          ],
-          colors: ["#1A56DB", "#FDBA8C"],
-          chart: {
-              type: 'bar',
-              height: 350,
-              toolbar: {
-                  show: false,
-              },
-          },
-          plotOptions: {
-              bar: {
-                  horizontal: false,
-                  columnWidth: "50%",
-                  borderRadiusApplication: "end",
-                  borderRadius: 45,
-              }
-          },
-          grid: {
-              show: false
-          },
-          dataLabels: {
-              enabled: false
-          },
-          xaxis: {
-              categories: ['Bill'],
-          }
-      };
-
-      this.barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartoptions);
-      this.barChart.render();
-
-      // Update the bar chart when the slider value changes
-      slider.addEventListener('change', this.updateChart);
-
-      const pieChartOptions = {
-          series: [30, 76],
-          chart: {
-              type: 'pie',
-              height: 325,
-
-          },
-          colors: ["#1A56DB", "#FDBA8C"],
-          legend: {
-              show: false
-          },
-          dataLabels: {
-              enabled: false
-          },
-          responsive: [{
-              breakpoint: 480,
-              options: {
-                  chart: {
-                      width: 325
-                  }
-              }
-          }]
-      };
-
-      const pieChart = new ApexCharts(document.querySelector("#pie-chart"), pieChartOptions);
-      pieChart.render();
+                },
+                colors: ["#1A56DB", "#FDBA8C"],
+                legend: {
+                    show: false
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 325
+                        }
+                    }
+                }]
+            }
+        };
     },
-    updateChart() {
-      const slider = document.getElementById('myRange');
-      const year = parseInt(slider.value);
-      
-      if (year == 0) {
-        this.currentCostPerKwh = 0.247;
-      } else if (year == 5) {
-        this.currentCostPerKwh = 0.301;
-      } else if (year == 10) {
-        this.currentCostPerKwh = 0.366;
-      } else if (year == 15) {
-        this.currentCostPerKwh = 0.445;
-      } else if (year == 20) {
-        this.currentCostPerKwh = 0.542;
-      } else if (year == 25) {
-        this.currentCostPerKwh = 0.659;
-      }
+    mounted() {
+        const slider = document.getElementById('myRange');
+        const labelsContainer = document.getElementById('sliderLabels');
 
-      const billPerMonth = Math.round(this.currentCostPerKwh * 486); // Consumo mensual en kWh
-      this.currentBill = billPerMonth;
-      
-      // Update the series data
-      this.barChart.updateSeries([
-        { name: "Current Bill", data: [billPerMonth] },
-        { name: "Your Payment", data: [this.yourPayment] }
-      ]);
+        const min = parseInt(slider.min);
+        const max = parseInt(slider.max);
+        const step = (max - min) / 5;  // Divide into 6 parts
+
+        for (let i = min; i <= max; i += step) {
+            const li = document.createElement('li');
+            li.className = 'flex justify-center relative';
+            li.innerHTML = `<span class="absolute">${Math.round(i)}</span>`;
+            labelsContainer.appendChild(li);
+        }
+
+        this.barChart = new ApexCharts(document.querySelector("#bar-chart"), this.barChartoptions);
+        this.barChart.render();
+
+        // Update the bar chart when the slider value changes
+        slider.addEventListener('change', this.updateChart);
+
+        const pieChart = new ApexCharts(document.querySelector("#pie-chart"), this.pieChartOptions);
+        pieChart.render();
+    },
+    methods: {
+        updateChart() {
+            const slider = document.getElementById('myRange');
+            const year = parseInt(slider.value);
+
+            if (year == 0) {
+                this.currentCostPerKwh = 0.247;
+            } else if (year == 5) {
+                this.currentCostPerKwh = 0.301;
+            } else if (year == 10) {
+                this.currentCostPerKwh = 0.366;
+            } else if (year == 15) {
+                this.currentCostPerKwh = 0.445;
+            } else if (year == 20) {
+                this.currentCostPerKwh = 0.542;
+            } else if (year == 25) {
+                this.currentCostPerKwh = 0.659;
+            }
+
+            const billPerMonth = Math.round(this.currentCostPerKwh * 486); // Consumo mensual en kWh
+            this.currentBill = billPerMonth;
+
+            // Update the series data
+            this.barChart.updateSeries([
+                { name: "Current Bill", data: [billPerMonth] },
+                { name: "Your Payment", data: [this.yourPayment] }
+            ]);
+        }
     }
-  }
 }
 </script>
 
--- src/pages/ClientsForm.vue
+++ src/pages/ClientsForm.vue
@@ -7,74 +7,61 @@
         <template v-slot:body>
             <form class="grid grid-cols-2 gap-4">
                 <!-- Name -->
-                <div class="relative" data-te-input-wrapper-init>
-                    <input type="text"
-                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                        id="Name" placeholder="Example label" style="outline: none; border: 1px solid #cccccc;" />
-                    <label for="Name"
-                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] 
-                      text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-                      peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:bg-light z-1
-                       peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:bg-light">
+                <div class="relative">
+                    <input type="text" id="small_outlined"
+                        class="block px-2.5 pb-1 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " />
+                    <label for="small_outlined"
+                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-light dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">
                         First name
                     </label>
                 </div>
                 <!-- Last name -->
                 <div class="relative" data-te-input-wrapper-init>
-                    <input type="text"
-                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                        id="LastName" placeholder="Example label" style="outline: none; border: 1px solid #cccccc;" />
-                    <label for="LastName"
-                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] 
-                      text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-                      peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:bg-light z-1
-                       peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:bg-light">
+                    <input type="text" id="small_outlined"
+                        class="block px-2.5 pb-1 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " />
+                    <label for="small_outlined"
+                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-light dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">
                         Last name
                     </label>
                 </div>
                 <!-- Phone -->
                 <div class="relative" data-te-input-wrapper-init>
-                    <input type="text"
-                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                        id="phone" placeholder="Example label" style="outline: none; border: 1px solid #cccccc;" />
-                    <label for="phone"
-                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] 
-                      text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-                      peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:bg-light z-1
-                       peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:bg-light">
+                    <input type="text" id="small_outlined"
+                        class="block px-2.5 pb-1 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " />
+                    <label for="small_outlined"
+                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-light dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">
                         Phone
                     </label>
                 </div>
                 <!-- Email -->
                 <div class="relative" data-te-input-wrapper-init>
-                    <input type="text"
-                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                        id="phone" placeholder="Example label" style="outline: none; border: 1px solid #cccccc;" />
-                    <label for="phone"
-                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] 
-                        text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-                        peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:bg-light z-1
-                        peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary peer-focus:bg-light">
+                    <input type="text" id="small_outlined"
+                        class="block px-2.5 pb-1 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " />
+                    <label for="small_outlined"
+                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-light dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1">
                         Email
                     </label>
                 </div>
                 <!-- status -->
                 <div class="relative" data-te-input-wrapper-init>
-                    <div className="mb-3 xl:w-96">
+                    <div className="mb-3">
                         <select className="form-select appearance-none
-                                block
                                 w-full
                                 px-3
-                                py-1.5
-                                text-base
+                                pb-2
+                                pt-2
+                                text-sm
                                 font-normal
-                                text-gray-700
+                                text-gray-500
                                 bg-light bg-clip-padding bg-no-repeat
                                 border border-solid border-gray-300
-                                rounded
+                                rounded-lg
                                 transition
                                 ease-in-out
-                                m-0
                                 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             aria-label="Default select example">
                             <option selected>Select status...</option>
